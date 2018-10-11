d0627cc71beb8edcc555484c5310725d2ac3c918
Jeremy Emata <jeremy.emata@gmail.com>
2018-10-11 08:33:24 +0000

    automatically backed up by learn

--- lib/musicimportercontroller.rb
+++ lib/musicimportercontroller.rb
@@ -66,7 +66,7 @@ binding.pry
 def list_songs_by_genre 
     puts "Please enter the name of a genre:"
     input = gets.strip
-
+binding.pry 
     if genre = Genre.find_by_name(input)
       genre.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
         puts "#{i}. #{s.artist.name} - #{s.name}"
